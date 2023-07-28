3a501b216327b716b06d0416066a4895e55dc61a
manas <manaschubby@gmail.com>
2023-07-28 15:56:09 +0530

    First Commit

---
+++ MyFirstApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace MyFirstApp;
+
+public partial class MainPage : ContentPage
+{
+    string translatedNumber;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        Console.Write(enteredNumber);
+        translatedNumber = Translator.ToNumber(enteredNumber);
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.Text = "Call " + translatedNumber;
+            CallButton.IsEnabled = true;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+    private async void OnCall(object sender, EventArgs e) {
+        if(await this.DisplayAlert("Dial a Number",
+        "Would you like to call " + translatedNumber + "?",
+        "Yes",
+        "No")){
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                {
+                    PhoneDialer.Default.Open(translatedNumber);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to Call", "Invalid Phone Number", "OK");
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Unable to Call", "Phone Dialing Failed", "OK");
+            }
+        }
+    }
+
+}
+
+
---
+++ MyFirstApp/Translator.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+namespace MyFirstApp;
+
+public static class Translator
+{
+	public static string ToNumber(string raw)
+	{
+		if (string.IsNullOrWhiteSpace(raw))
+		{
+			return null;
+		}
+		raw = raw.ToUpperInvariant();
+		var number = new StringBuilder();
+		foreach (var c in raw)
+		{
+			if ("-0123456789".Contains(c))
+			{
+				number.Append(c);
+			}
+			else
+			{
+				var result = TranslateToNumber(c);
+				if (result != null)
+				{
+					number.Append(result);
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+		return number.ToString();
+	}
+	static readonly string[] digits =
+	{
+        "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+    };
+	static int? TranslateToNumber(char c)
+	{
+		for (int i =0; i<digits.Length; i++)
+		{
+			if (digits[i].Contains(c))
+				return i + 2;
+		}
+		return null;
+	}
+}
+
